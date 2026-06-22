24ec7b52213de5ad377f764754034cba30be55a1
DerekAJanzen <154301921+DerekAJanzen@users.noreply.github.com>
2026-06-21 19:46:00 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //new solution for O(1) space after looking at docs
+        int l = 0;
+        int r = s.length()-1;
+        while(l < r){
+            while(l < r && !Character.isAlphabetic(s.charAt(r)) && !Character.isDigit(s.charAt(r))) r--;
+            while(l < r && !Character.isAlphabetic(s.charAt(l)) && !Character.isDigit(s.charAt(l))) l++;
+            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
