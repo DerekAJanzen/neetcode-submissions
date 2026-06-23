50dc0ee42a31d5d5cb11d7f5a0bb83a11602a9bf
DerekAJanzen <154301921+DerekAJanzen@users.noreply.github.com>
2026-06-22 18:30:07 -0700

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int l = 0;
-        int r = 1;
-        int max = 0;
-
-        while(r < prices.length){
-            if(prices[r] < prices[l]){
+       int profit = 0;
+       int l = 0;
+       int r = 1;
+       while(r < prices.length){
+            profit = Math.max(profit, prices[r] - prices[l]);
+            if(prices[l] > prices[r]){
                 l = r;
+                r++;
+            } else{
+                r++;
             }
-            max = Math.max(max, prices[r] - prices[l]);
-            r++;
-        }
-        return max;
+       }
+       return profit;
     }
 }
